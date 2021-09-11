511b810355e83b5ad6b47d341e7be7b257432337
israel1jol <68302856+israel1jol@users.noreply.github.com>
2021-09-11 06:39:00 +0100

    update info.js

--- pages/info.js
+++ pages/info.js
@@ -8,8 +8,8 @@ const info = () => {
             </Head>
             <div>
                 <article>Your cummulated Grade Point Average is Caclculated by 
-                    <p>1.)Multiplying each individaul Grade Score by their associated units. </p>
-                    <p>2.)Summing the Weighted Grade Points.</p>
+                    <p>1.) Multiplying each individual Grade Score by their associated units. </p>
+                    <p>2.) Summing the Weighted Grade Points.</p>
                     <p>3.) Adding the Units for all the courses.</p>
                     <p>4.) Dividing the Sum of the weighted grade points by the total units</p>
                 </article>
